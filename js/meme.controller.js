d4ccf4606d23523bd032199f748897ee31355b6e
Gruper9 <150256966+Gruper9@users.noreply.github.com>
2023-11-29 19:11:10 +0200

    add initial funtionality and data base

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -1,1 +1,47 @@
 'use strict'
+
+let gElCanvas
+let gCtx
+
+function onInit() {
+    gElCanvas = document.querySelector('canvas')
+    gCtx = gElCanvas.getContext('2d')
+    renderGalery()
+}
+
+
+function renderGalery() {
+    const elGalery = document.querySelector('.img-galerey')
+    const galery = getGalery()
+    elGalery.innerHTML = galery.map(img => `<img src="${img.url}" onclick="coverCanvasWithImg(this)" data-id="${img.id}">`).join('')
+}
+
+
+function onDraw(ev) {
+    const { offsetX, offsetY } = ev
+    drawText(getLine(), offsetX, offsetY)
+}
+
+function drawText(text, x, y) {
+    gCtx.lineWidth = 2
+    gCtx.strokeStyle = 'black'
+    gCtx.fillStyle = getLineColor()
+    gCtx.font = `${getLineSize()}px Arial`
+    gCtx.textAlign = 'center'
+    gCtx.textBaseline = 'middle'
+    gCtx.fillText(text, x, y)
+    gCtx.strokeText(text, x, y)
+}
+
+function coverCanvasWithImg(elImg) {
+   const imgId= returnImgById(elImg.dataset.id)
+    setSelectedImg(imgId)
+    gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+}
+
+function onEditLine(elInput){
+    setLine(elInput.value)
+}
+function onSetColor(elInput){
+    setColor(elInput.value)
+}
--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -1,1 +1,56 @@
 'use strict'
+
+
+const gImgs = [{ id: 1, url: 'imgs/1.jpg', keywords: ['baby', 'strong'] },
+{ id: 2, url: 'imgs/2.jpg', keywords: ['funny', 'man'] },
+{ id: 3, url: 'imgs/3.jpg', keywords: ['man', 'toast', 'movie'] },
+{ id: 4, url: 'imgs/4.jpg', keywords: ['man', 'movie'] },
+{ id: 5, url: 'imgs/5.jpg', keywords: ['man', 'funny', 'movie'] },
+{ id: 6, url: 'imgs/6.jpg', keywords: ['funny', 'movie'] }
+]
+
+
+var gMeme = {
+    selectedImgId: 1,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'I sometimes eat Falafel',
+            size: 20,
+            color: 'red'
+        }
+    ]
+}
+var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
+
+
+function getSelectedImg() {
+    return gMeme.selectedImgId
+}
+function getGalery() {
+    return gImgs
+}
+
+function setSelectedImg(id) {
+    gMeme.selectedImgId = id
+    console.log(gMeme.selectedImgId);
+}
+
+function returnImgById(id) {
+    return gImgs[id - 1]
+}
+function getLine() {
+    return gMeme.lines[0].txt
+}
+function setLine(txt) {
+    gMeme.lines[0].txt = txt
+}
+function getLineColor() {
+    return gMeme.lines[0].color
+}
+function getLineSize() {
+    return gMeme.lines[0].size
+}
+function setColor(color) {
+    gMeme.lines[0].color = color
+}
